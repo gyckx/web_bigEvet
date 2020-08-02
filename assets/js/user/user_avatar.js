a1654ec15882964d9c8ecd39f4e8975ce8e8779f
geyao <1054446465@qq.com>
2020-08-02 11:01:28 +0800

    ok

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -29,10 +29,6 @@ $(function () {
         var fileList = e.target.files
         if (fileList.length === 0) {
             return layer.msg('请选择照片!')
-<<<<<<< HEAD
-            console.log(222);
-=======
->>>>>>> user
         }
 
         //1.拿到用户选择的文件  target.files[0]: 指选中的图片
@@ -43,21 +39,12 @@ $(function () {
         $image
             .cropper('destroy') //销毁旧的裁剪区域
             .attr('src', imgURL) //重新设置图片路径  自定义属性
-<<<<<<< HEAD
             .cropper(options) //重新初始化裁剪区域
     })
 
     // 为确定按钮设置点击事件 将修剪后的头像上传到服务器
     $('#btnUpload').on('click', function () {
         // 1. 获取到用户修改后的头像
-=======
-            .cropper(options) //重新初始化裁剪区
-    })
-
-    // 为确认按钮绑定点击事件将图片上传到服务器
-    $('#btnUpload').on('click', function () {
-        // 1. 拿到用户裁剪之后的头像
->>>>>>> user
         var dataURL = $image
             .cropper('getCroppedCanvas', {
                 // 创建一个 Canvas 画布
@@ -65,21 +52,14 @@ $(function () {
                 height: 100
             })
             .toDataURL('image/png')
-<<<<<<< HEAD
         // 2. 将拿到的数据发送到服务器上
         $.ajax({
             type: 'post',
-=======
-        // 2. 把头像上传到服务器
-        $.ajax({
-            method: 'POST',
->>>>>>> user
             url: '/my/update/avatar',
             data: {
                 avatar: dataURL
             },
             success: function (res) {
-<<<<<<< HEAD
                 // 返回校验
                 if (res.status !== 0) {
                     return layer.msg('头像更新失败')
@@ -94,16 +74,6 @@ $(function () {
 
 
 
-=======
-                if (res.status !== 0) {
-                    return layer.msg('更换头像失败！')
-                }
-                layer.msg('更换头像成功！')
-                window.parent.getUserInfo()
-            }
-        })
-
->>>>>>> user
     })
 
 
--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,5 +1,4 @@
 $(function () {
-<<<<<<< HEAD
 
     // 1. 用户校验
     var form = layui.form;
@@ -14,27 +13,12 @@ $(function () {
     })
     // 2. 初始化用户信息
     initUserInfo();
-=======
-    //1.alert-success 校验规则
-    var form = layui.form;
-    var layer = layui.layer
-    form.verify({
-        nickname: function (value) {
-            if (value.trim().length > 6) {
-                return '昵称不能大于6个字符';
-            }
-        }
-    })
-    initUserInfo();
-    //2. 初始化用户信息
->>>>>>> user
     function initUserInfo() {
         $.ajax({
             type: 'get',
             url: '/my/userinfo',
             success: function (res) {
                 if (res.status !== 0) {
-<<<<<<< HEAD
                     return layer.msg('数据获取失败')
                 }
                 // 给表单赋值
@@ -50,27 +34,6 @@ $(function () {
         // 将信息重新初始化
         initUserInfo();
     })
-=======
-                    return layer.msg('获取数据失败')
-                }
-                // console.log(res);
-                // 给表单赋 原生代码：获取dom表单属性进行赋值
-                form.val('formUserInfo', res.data)
-            }
-        })
-
-    }
-
-    // 3. 完成重置按钮功能
-    $('#btnReset').on('click', function (e) {
-        // 阻止表单提交行为
-        e.preventDefault();
-        // 数据的表单重新渲染
-        initUserInfo();
-
-    })
-
->>>>>>> user
     // 4. 设置监听表单的行为
     $('.layui-form').on('submit', function (e) {
         // 阻止表单提交的默认行为
@@ -84,14 +47,8 @@ $(function () {
                     return layer.msg('数据跟新失败')
                 }
                 layer.msg('数据跟新成功');
-<<<<<<< HEAD
                 //将侧边栏的头像的信息进行更新 
                 //使用父盒子中的window方法中的
-=======
-                // 使用父框架中的window 然后显示头像信息
-                // fm 中的html文件是 子页面 还有一个是inde的主页面 两者嵌套在一起
-                // 加载侧边栏的头像信息在主页面的window中 
->>>>>>> user
                 window.parent.getUserInfo();
             }
         })
@@ -100,8 +57,4 @@ $(function () {
 
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> user
 })
