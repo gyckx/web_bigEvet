b9827661665eb55f47e33cbf4324954a96199427
geyao <1054446465@qq.com>
2020-08-02 14:29:00 +0800

    ok

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,9 +1,11 @@
 $(function () {
 
+    // 为了弹出层申明layer
+    var layer = layui.layer;
+
     // 将数据库中的数据渲染出来
     initArtCateLit();
     function initArtCateLit() {
-
         $.ajax({
             type: 'get',
             url: '/my/article/cates',
@@ -12,11 +14,19 @@ $(function () {
                 $('tbody').html(strHtml)
             }
         })
-
-
-
-
     }
+
+    // 给添加类别按钮设置事件 并且设置弹出层
+    $('#btnAddCate').on('click', function () {
+        layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章类别',
+            content: $('#dialog-add').html()
+        })
+
+
+    })
 
 
 
